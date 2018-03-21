eb1d2238e169682ffed7645291d5598996ff5494
Sebastian Springer <info@sebastian-springer.com>
2018-03-21 09:53:26 +0100

    modularisierung

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const http = require('http');
+const myModule = require('./module');
 
-http
-  .createServer((req, res) => {
-    debugger;
-    console.log(req.url);
-    res.end('Hallo client');
-  })
-  .listen(8080, () => {
-    console.log('Server is listening');
-  });
+http.createServer(myModule.server).listen(8080, () => {
+  console.log('Server is really listening');
+});
 
 // github.com/sspringer82/qs
+
+// nodemon.io
+// npm install -g nodemon
