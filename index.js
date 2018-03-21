a87a63c442a2d0d8fb0df88e9854cfdb203188cf
Sebastian Springer <info@sebastian-springer.com>
2018-03-21 09:34:20 +0100

    debugging

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const http = require('http');
 
 http
   .createServer((req, res) => {
+    debugger;
+    console.log(req.url);
     res.end('Hallo client');
   })
   .listen(8080, () => {
     console.log('Server is listening');
   });
+
+// github.com/sspringer82/qs
